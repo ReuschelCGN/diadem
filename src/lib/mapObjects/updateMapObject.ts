a6662a2dfdb6f1ce2afa97f5ce50bfa28ab2e0b3
Malte <42342921+ccev@users.noreply.github.com>
2026-03-26 00:26:21 +0100

    chore: disable route functionality for now

--- src/lib/mapObjects/updateMapObject.ts
+++ src/lib/mapObjects/updateMapObject.ts
@@ -65,6 +65,7 @@ export async function updateMapObject(
 	signal?: AbortSignal
 ) {
 	if (!hasFeatureAnywhere(getUserDetails().permissions, type)) return;
+	if (type === MapObjectType.ROUTE) return
 
 	let filter: AnyFilter | undefined = undefined;
 
