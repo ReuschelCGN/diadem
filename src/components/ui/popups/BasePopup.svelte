1c269c86847926f69bd590d34e134bba68c45082
Malte <42342921+ccev@users.noreply.github.com>
2026-05-05 00:45:15 +0200

    feat: More actions in Popups (#140)
    * rework popup buttons
    
    * refactor expandedPopups -> popupActions
    
    * prepare usersettings for actions
    
    * make popup action buttons functional
    
    * implement dimming pokemon
    
    * add uicon index error handling
    
    * dim &  range support
    
    * show updated locations on map
    
    * implement share button & style fixes
    
    * add actions to other map objects, cleanups
    
    * timer layer cleanup
    
    * TimerLayer fixes
    
    * clean up TimerLayer
    
    * missing translations and small fixes
    
    * improver TimeLayer performance
    
    * fix modifiers

--- src/components/ui/popups/BasePopup.svelte
+++ src/components/ui/popups/BasePopup.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
-	import { isPopupExpanded } from "@/lib/ui/expandedPopups.js";
+	import { isPopupExpanded } from "@/lib/ui/popupActions.js";
 	import PopupButtons from "@/components/ui/popups/common/PopupButtons.svelte";
 	import Card from "@/components/ui/Card.svelte";
-	import { X } from "lucide-svelte";
+	import { Copy, Share2, X } from "lucide-svelte";
 	import type { Snippet } from "svelte";
 	import { slide } from "svelte/transition";
 	import { cubicIn, cubicOut } from "svelte/easing";
 	import Button from "@/components/ui/input/Button.svelte";
 	import CloseButton from "@/components/ui/CloseButton.svelte";
-	import { closePopup } from "@/lib/mapObjects/interact";
+	import { closePopup, getCurrentPath } from "@/lib/mapObjects/interact";
 	import { getCurrentSelectedData } from "@/lib/mapObjects/currentSelectedState.svelte";
+	import { backupShareUrl, canNativeShare, copyToClipboard, hasClipboardWrite } from "@/lib/utils/device";
+	import { getLocale } from "@/lib/paraglide/runtime";
+	import * as m from "@/lib/paraglide/messages";
 
 	let {
 		lat,
@@ -28,14 +31,45 @@
 		description?: Snippet;
 		content?: Snippet;
 	} = $props();
+
+	function getShareUrl() {
+		return window.location.origin + getCurrentPath() + "?lang=" + getLocale();
+	}
 </script>
 
-<Card class="h-full relative overflow-hidden py-4 mx-2 @container">
-	<CloseButton class="absolute right-2 top-2" onclick={closePopup} />
+<Card class="h-full relative overflow-hidden pt-4 mx-2">
+
+	<div class="absolute right-2 top-3 flex gap-1.5">
+		{#if canNativeShare({ url: getShareUrl() })}
+			<Button
+				variant="ghost"
+				size=""
+				class="rounded-full size-8 p-2"
+				title={m.popup_share()}
+				onclick={() => backupShareUrl(getShareUrl())}
+			>
+				<Share2 class="size-3.5" />
+			</Button>
+		{:else if hasClipboardWrite()}
+			<Button
+				variant="ghost"
+				size=""
+				class="rounded-full size-8 p-2"
+				title={m.copy_link()}
+				onclick={() => copyToClipboard(getShareUrl())}
+			>
+				<Copy class="size-3.5" />
+			</Button>
+		{/if}
+		<Button variant="ghost" size="" class="rounded-full p-2 size-8" title={m.close()} onclick={closePopup}>
+			<X class="size-4.5" />
+		</Button>
+	</div>
+
 	<div class="flex pl-6 pr-3 w-full items-center mb-2">
 		{@render image()}
 		<div class="w-full h-fit ml-4 max-h-full">
-			<div class="mr-9">
+			<div class="mr-16">
 				{@render title()}
 			</div>
 
@@ -53,7 +87,7 @@
 
 	{#if isPopupExpanded(getCurrentSelectedData()?.type)}
 		<div
-			class="px-6 overflow-y-auto"
+			class="px-6 overflow-y-auto mb-2"
 			style="max-height: calc(100vh - 26rem);"
 			in:slide={{ duration: 90, easing: cubicIn }}
 			out:slide={{ duration: 90, easing: cubicOut }}
@@ -62,6 +96,5 @@
 		</div>
 	{/if}
 
-	<div class="h-12"></div>
 	<PopupButtons {lat} {lon} />
 </Card>
